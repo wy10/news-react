23acfbc7c3d6e7c10d6b32c43d43e0e2c6c8ccc5
YANWEN <wenyan@91yjd.com>
2019-03-05 17:15:05 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+123
+        
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
